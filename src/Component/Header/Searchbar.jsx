b7ecd63463f330721527a79c4314112ccf4faa97
HosseinBagheri <hossein9217bagheri@gmail.com>
2025-06-08 03:30:17 -0700

    responsive input

--- src/Component/Header/Searchbar.jsx
+++ src/Component/Header/Searchbar.jsx
@@ -6,7 +6,7 @@ export const Searchbar = () => {
         <h2 className='text-3xl font-bold md:text-5xl'>Welcome.</h2>
         <h2 className='mb-10 text-lg font-bold md:text-2xl'>Millions of movies, TV shows and people to discover. Explore now.</h2>
         <div className='relative'>
-            <input type="text" placeholder='Search for a movie, tv show, person......' className='w-full px-6 py-3 text-black bg-slate-400 outline-none rounded-3xl placeholder:text-black placeholder:text-sm md:placeholder:text-lg'/>
+            <input type="text" placeholder='Search for a movie, tv show, person......' className='w-full px-6 py-3 text-black bg-slate-400 outline-none rounded-3xl placeholder:text-black placeholder:text-xs md:placeholder:text-lg'/>
             <a className='absolute right-0 px-8 py-3 text-black rounded-3xl bg-slate-600 ' href="#">Search</a>
         </div>
     </div>
